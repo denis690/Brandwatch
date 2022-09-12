f69c92b4752de22811eedca03ecbf118d5acecae
denis gershengoren <denis@nexudus.com>
2022-09-12 09:34:41 +0100

    Initial Commit

---
+++ src/test/java/utils/UrlMapping.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class UrlMapping {
+
+    public static final String WEB_DOMAIN_URL = "https://gorest.co.in";
+
+    //V2
+    public static final String USERS_V2_URL = WEB_DOMAIN_URL + "/public/v2/users";
+
+    public static final String POSTS_V2_URL = WEB_DOMAIN_URL + "/public/v2/posts";
+
+    public static final String COMMENTS_V2_URL = WEB_DOMAIN_URL + "/public/v2/comments";
+
+    public static final String TODO_V2_URL = WEB_DOMAIN_URL + "/public/v2/todos";
+
+
+}
